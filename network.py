1a22fbda5b1077962806b66a8dc3e54d59ec14d1
Heuzef <contact@heuzef.com>
2024-03-21 11:32:16 +0100

    Update network.py

--- network.py
+++ network.py
@@ -7,31 +7,31 @@ with Diagram("Carte reseau de Heuzef - %s" % today.strftime('%d/%m/%Y %H:%M'), s
 
 	# Custom icons
 	internet = Custom("Internet", "icons/internet.png")
-	router = Custom("Routeur Free \n 192.168.0.1", "icons/router.png")
-	devices = Custom("Peripheriques \n Range DHCP", "icons/devices.png")
-	pgmr = Custom("PGMR \n 192.168.0.X", "icons/computer.png")
-	bmc = Custom("BMC \n 192.168.0.2", "icons/controls.png")
-	pve = Custom("PVE \n 192.168.0.100", "icons/server.png")
-	wifi = Custom("Wi-Fi \n 192.168.0.200/24", "icons/wifi.png")
-	imprimante = Custom("Imprimante \n 192.168.0.201 \n ", "icons/print.png")
-	brix = Custom("Brix \n 192.168.0.202", "icons/cpu.png")
-	aura = Custom("Aura \n 192.168.0.203", "icons/clock.png")
-	mk4 = Custom("mk4.heuzef.com \n 192.168.0.204", "icons/prusa.png")
-	switch = Custom("Switch \n 192.168.0.239", "icons/switch.png")
-	proxy = Custom("[VM] \n proxy \n 192.168.0.101", "icons/networking-app-gw.png")
-	vault = Custom("[VM] \n vault.heuzef.com \n 192.168.0.103", "icons/bitwarden.png")
-	git = Custom("[VM] \n git.heuzef.com \n 192.168.0.104", "icons/gitea.png")
-	cognhacker_net = Custom("[VM] \n cognhacker.net \n 192.168.0.105", "icons/cognhacker.png")
-	heuzef_link = Custom("[VM] \n heuzef.link \n 192.168.0.106", "icons/shlink.png")
-	files = Custom("[VM] files \n 192.168.0.110", "icons/files.png")
-	media = Custom("[VM] media \n 192.168.0.111", "icons/media-services.png")
-	backup = Custom("[VM] \n backup \n 192.168.10.114", "icons/borg.png")
-	www = Custom("[VM] \n heuzef.com \n 192.168.0.120", "icons/web.png")
-	lemurier_immo = Custom("[VM] \n lemurier.immo \n 192.168.0.121", "icons/web.png")
-	couvreur_cognac_fr = Custom("[VM] \n couvreur-cognac.fr \n 192.168.0.122", "icons/web.png")
-	beboop_boo = Custom("[VM] \n beboop.boo \n 192.168.0.123", "icons/web.png")
-
-	# with Cluster("SPARE"):
+	with Cluster("LAN"):
+		router = Custom("Routeur Free \n 192.168.0.1", "icons/router.png")
+		devices = Custom("Peripheriques \n Range DHCP", "icons/devices.png")
+		pgmr = Custom("PGMR \n 192.168.0.X", "icons/computer.png")
+		bmc = Custom("BMC \n 192.168.0.2", "icons/controls.png")
+		pve = Custom("PVE \n 192.168.0.100", "icons/server.png")
+		wifi = Custom("Wi-Fi \n 192.168.0.200/24", "icons/wifi.png")
+		imprimante = Custom("Imprimante \n 192.168.0.201 \n ", "icons/print.png")
+		brix = Custom("Brix \n 192.168.0.202", "icons/cpu.png")
+		aura = Custom("Aura \n 192.168.0.203", "icons/clock.png")
+		switch = Custom("Switch \n 192.168.0.239", "icons/switch.png")
+		media = Custom("[VM] media \n 192.168.0.111", "icons/media-services.png")
+		backup = Custom("[VM] \n backup \n 192.168.10.114", "icons/borg.png")
+		with Cluster("REVERSE-PROXY"):
+			proxy = Custom("[VM] \n proxy \n 192.168.0.101", "icons/networking-app-gw.png")
+			vault = Custom("[VM] \n vault.heuzef.com \n 192.168.0.103", "icons/bitwarden.png")
+			git = Custom("[VM] \n git.heuzef.com \n 192.168.0.104", "icons/gitea.png")
+			cognhacker_net = Custom("[VM] \n cognhacker.net \n 192.168.0.105", "icons/cognhacker.png")
+			heuzef_link = Custom("[VM] \n heuzef.link \n 192.168.0.106", "icons/shlink.png")
+			files = Custom("[VM] files.heuzef.com \n 192.168.0.110", "icons/files.png")
+			www = Custom("[VM] \n heuzef.com \n 192.168.0.120", "icons/web.png")
+			lemurier_immo = Custom("[VM] \n lemurier.immo \n 192.168.0.121", "icons/web.png")
+			couvreur_cognac_fr = Custom("[VM] \n couvreur-cognac.fr \n 192.168.0.122", "icons/web.png")
+			beboop_boo = Custom("[VM] \n beboop.boo \n 192.168.0.123", "icons/web.png")
+			mk4 = Custom("mk4.heuzef.com \n 192.168.0.204", "icons/prusa.png")
 
 	# Reverse Proxy
 	proxy - mk4
@@ -47,6 +47,12 @@ with Diagram("Carte reseau de Heuzef - %s" % today.strftime('%d/%m/%Y %H:%M'), s
 
 	# Links
 	internet >> Edge(color="red", label="FTTH", style="bold") >> router >> Edge(color="red", style="bold") >> proxy
+	routeur << Edge(color="red", style="bold") << wifi
+	routeur << Edge(color="red", style="bold") << bmc
+	routeur << Edge(color="red", style="bold") << pve
+	routeur << Edge(color="red", style="bold") << switch
+	routeur << Edge(color="red", style="bold") << media
+	routeur << Edge(color="red", style="bold") << backup
 	wifi << Edge(color="blue", style="dotted") << devices
 	wifi << Edge(color="blue", style="dotted") << imprimante
 	wifi << Edge(color="blue", style="dotted") << brix
