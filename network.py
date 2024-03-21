36fc8c8f361f69f855807fc132de6d95335ead48
Heuzef <contact@heuzef.com>
2024-03-21 12:02:43 +0100

    Update network.py

--- network.py
+++ network.py
@@ -7,7 +7,7 @@ with Diagram("Carte reseau de Heuzef - %s" % today.strftime('%d/%m/%Y %H:%M'), s
 
 	# Custom icons
 	internet = Custom("Internet", "icons/internet.png")
-	hertzner = Custom("Hetzner", "icons/hetzner.png")
+	hetzner = Custom("Hetzner", "icons/hetzner.png")
 	with Cluster("LAN"):
 		router = Custom("Routeur Free \n 192.168.0.1", "icons/router.png")
 		devices = Custom("Peripheriques \n Range DHCP", "icons/devices.png")
@@ -48,8 +48,7 @@ with Diagram("Carte reseau de Heuzef - %s" % today.strftime('%d/%m/%Y %H:%M'), s
 
 	# Links
 	internet >> Edge(color="red", label="FTTH", style="bold") >> router >> Edge(color="blue", style="bold") >> proxy
-	internet >> Edge(color="green", style="bold") >> hertzner
-	internet << Edge(color="green", style="bold") << router << Edge(color="green", style="bold") >> backup
+	hetzner << Edge(color="green", style="bold") >> backup
 	router << Edge(color="red", style="bold") << wifi
 	router << Edge(color="red", style="bold") << bmc
 	router << Edge(color="red", style="bold") << pve
